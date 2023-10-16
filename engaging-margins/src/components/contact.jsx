7bbb2c9746ba3c25937b39574b05dd4776ccc586
ebtesam25 <ehaque4@gmu.edu>
2023-10-16 14:44:40 -0400

    font size updates

--- engaging-margins/src/components/contact.jsx
+++ engaging-margins/src/components/contact.jsx
@@ -3,7 +3,7 @@ export default function Contact() {
     return(
         <div className='py-10 px-20 bg-secondary' id="contact">
         <h1 className='text-5xl font-bold'>Contact</h1>
-        <p className="text-3xl my-5">Questions? Contact us at <a href="mailto:johnsonb@gmu.edu" className="font-bold">johnsonb@gmu.edu</a> or <a href="mailto:angela.smith@ischool.utexas.edu"  className="font-bold">angela.smith@ischool.utexas.edu</a></p>
+        <p className="text-2xl my-5">Questions? Contact us at <a href="mailto:johnsonb@gmu.edu" className="font-bold">johnsonb@gmu.edu</a> or <a href="mailto:angela.smith@ischool.utexas.edu"  className="font-bold">angela.smith@ischool.utexas.edu</a></p>
         
         </div>
         )
