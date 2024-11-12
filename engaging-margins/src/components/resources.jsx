71dd58e4bc5a1e80d25caea340abab0c8abf503c
ebtesam25 <ehaque4@gmu.edu>
2024-11-11 23:54:06 -0500

    added tech4black lives

--- engaging-margins/src/components/resources.jsx
+++ engaging-margins/src/components/resources.jsx
@@ -26,6 +26,19 @@ const Resources = () => {
 </svg>
             </a>
         </div>
+
+        <div className='py-2.5'></div>
+            <p className='text-2xl font-bold'>Related Projects</p>
+            <div className='flex flex-row'>
+            <p className='text-xl'>Technology for Black Lives</p>
+            <a href="https://go.gmu.edu/Tech4BlackLives" target="_blank" rel="noopener noreferrer" className='px-2.5'>
+            <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 22.0002H16C18.8284 22.0002 20.2426 22.0002 21.1213 21.1215C22 20.2429 22 18.8286 22 16.0002V15.0002C22 12.1718 22 10.7576 21.1213 9.8789C20.3529 9.11051 19.175 9.01406 17 9.00195M7 9.00195C4.82497 9.01406 3.64706 9.11051 2.87868 9.87889C2 10.7576 2 12.1718 2 15.0002L2 16.0002C2 18.8286 2 20.2429 2.87868 21.1215C3.17848 21.4213 3.54062 21.6188 4 21.749"/>
+            <path d="M16 5H8M16 5V13M16 5L7 14"/>
+            </svg>
+            </a>
+        </div>
+
         </div>
     );
 };
