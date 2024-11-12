8fcb9eca797d3107899df5a4914c6ae08faa5cd7
ebtesam25 <ehaque4@gmu.edu>
2024-11-12 05:40:08 -0500

    interview details added

--- engaging-margins/src/components/landing.jsx
+++ engaging-margins/src/components/landing.jsx
@@ -8,13 +8,16 @@ export default function Landing(){
 
     <div className="max-w-5xl bg-neutral rounded-2xl p-10 bg-opacity-70">
       <h1 className="mb-5 text-5xl font-bold">Engaging the Margins</h1>
-      <p className="mb-5 text-3xl">We are researchers interested in learning about your experiences building and using technology, with a focus on engagement with individuals who identify as Black, Indigenous, or People of Color (BIPOC) in technological spaces and datasets.</p>
+      <h2 className="mb-5 text-3xl font-bold">Are you a Technologist who engages with BIPOC in technological spaces and datasets?</h2>
+      <p className="mb-5 text-3xl">We are researchers interested in learning about your experiences building and using technology, with a focus on engagement with individuals who identify as Black, Indigenous, or Person of Color (BIPOC) in technological spaces and datasets..</p>
       <div className='flex content-center self-center place-content-center my-5'>
        
-    <p className='text-xl'><span className='font-bold'>Eligibility</span> <p>18 or older, engages with technology frequently</p></p></div>
+    <p className='text-xl'><span className='font-bold'>Eligibility</span> <p>18 years or older, seeks or collects data for professional use in technology</p></p></div>
     <div className='flex content-center self-center place-content-center my-5'>
-    <p className='text-xl'><span className='font-bold'>Time commitment</span> <p>10 mins</p></p></div>
-    <button className='btn btn-primary'><a href='https://utexas.qualtrics.com/jfe/form/SV_aUU2AaC9exSaIQK'>TAKE OUR SURVEY</a></button>
+    <p className='text-xl'><span className='font-bold'>Time commitment</span> <p>30-45 minute interview</p></p></div>
+    <div className='flex content-center self-center place-content-center my-5'>
+    <p className='text-xl'><span className='font-bold'>Compensation</span> <p>$40 Amazon gift card</p></p></div>
+    <button className='btn btn-primary'><a href='https://go.gmu.edu/etm_interview_tech'>SIGN UP HERE</a></button>
     </div>
 
   </div>
